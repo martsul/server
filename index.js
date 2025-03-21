41c82cd3106b53d3ae1ef54159c752195e89dc42
Aliaksandr Martsul <sasham9321@mail.ru>
2025-03-21 23:18:18 +0300

    change origin

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: process.env.FROM || "https://your-app-name.up.railway.app",
+        origin: "https://client-glcl.onrender.com",
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization", "Cache-Control"],
