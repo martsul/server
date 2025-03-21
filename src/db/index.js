2ff6d3c1e90034bf03a357915acff22f6422f7cc
Aliaksandr Martsul <sasham9321@mail.ru>
2025-03-21 19:06:59 +0300

    fix pool sql

--- src/db/index.js
+++ src/db/index.js
@@ -4,10 +4,10 @@ dotenv.config();
 
 export const pool = mysql
     .createPool({
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
+        host: "sql.freedb.tech",
+        port: 3306,
+        user: "freedb_qwera",
+        password: "rn@25VpJa?epTVa",
+        database: "freedb_users_app",
     })
     .promise();
