94237cd2da5e524741fa8416ebd8aac5803302ed
Aliaksandr Martsul <sasham9321@mail.ru>
2025-03-21 23:47:29 +0300

    change host

--- src/db/index.js
+++ src/db/index.js
@@ -4,10 +4,10 @@ dotenv.config();
 
 export const pool = mysql
     .createPool({
-        host: "sql.freedb.tech",
+        host: "sql7.freesqldatabase.com",
         port: 3306,
-        user: "freedb_qwera",
-        password: "rn@25VpJa?epTVa",
-        database: "freedb_users_app",
+        user: "sql7768912",
+        password: "hsB5i9TYqV",
+        database: "sql7768912",
     })
     .promise();
